aab00a3e47aa963f5b8678a552246b576703784e
Liutong Chen <liutong.clt@gmail.com>
2017-08-18 16:33:21 -0700

    posting data through postman

--- app.js
+++ app.js
@@ -10,40 +10,10 @@ var port = process.env.port || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+var bookRouter = require("./Routes/bookRoutes")(Book);
+app.use("/api/books", bookRouter);
+app.use("/api/authors", authorRouter);
 
-bookRouter.route('/Books')
-    .post(function(req, res) {
-        var book = new Book(req.body);
-        console.log(book);
-        res.send(book);
-    })
-    .get(function(req, res) {
-        var query = {};
-        if (req.query.genre) { //sanitize the query
-            query.genre = req.query.genre;
-        }
-        Book.find(query, function(err, books) {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.json(books);
-            }
-        });
-    });
-
-bookRouter.route("/Books/:bookId")
-    .get(function(req, res) {
-        Book.findById(req.params.bookId, function(err, book) {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.json(book);
-            }
-        });
-    })
-
-app.use("/api", bookRouter);
 
 app.get("/", function(req, res) {
     res.send("Welcome to my API!");
