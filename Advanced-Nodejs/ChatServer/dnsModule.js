2457a4235ccbac48bcdbd37a0b8bc866b9248686
Liutong Chen <liutong.clt@gmail.com>
2017-10-29 14:37:00 -0700

    Play with DNS module

---
+++ Advanced-Nodejs/ChatServer/dnsModule.js
@@ -0,0 +1,17 @@
+const dns = require('dns');
+
+dns.lookup('pluralsight.com', (err, address) => {
+    "use strict";
+    //this method uses libuv thread since not network is required
+    console.log('lookup: ', address);
+});
+
+dns.resolve('pluralsight.com', 'A', (err, address) => {
+    "use strict";
+    console.log('A record: ', address);
+});
+
+dns.reverse('35.162.130.220', (err, hostname) => {
+    "use strict";
+    console.log('hostName: ', hostname);
+});
